0ca71277c8ffeeab56052e1c19092d1f4e3f7cdd
James Ferguson <ncjamesferguson@gmail.com>
2018-05-24 21:20:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,3 +16,18 @@ function deepestChild(){
   }
   return node;
 }
+
+function increaseRankBy(n){
+  const rankedList = document.querySelectorAll('.ranked-list')
+
+  for ( let i = 0, l = rankedList.length; i < l ; i++){
+
+    let children = rankedList[i].children;
+
+    for ( let j = 0, k = children.length; j <k; j++) {
+
+      children[j].innerHTML = parseInt(children[j].innerHTML)+n
+
+    }
+  }
+}
